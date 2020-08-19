cb0cef99d03b20d9071213bed89d039738561bbc
Michael Rakitzis <michael.rakitzis@gmail.com>
2020-08-19 18:12:59 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -23,6 +23,6 @@ function doWhileLoop(num){
     return i;
   }
   do{
-    console.log("I run once regardless.")
+    console.log("I run once regardless.");
   } while (incrementVariable()< num);
 }
